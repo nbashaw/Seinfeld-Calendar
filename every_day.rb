b27e0dbdfd0178d17629290fffb0335517e3838c
Nathan Bashaw <nbashaw@gmail.com>
2011-01-02 18:16:40 -0500

    defined data models and included datamapper

--- every_day.rb
+++ every_day.rb
@@ -1,5 +1,34 @@
 require 'rubygems'
 require 'sinatra'
+require 'dm-core'
+require 'dm-migrations'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class User
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  has 1, :calendar
+end
+
+
+class Calendar
+  include DataMapper::Resource
+  property :id, Serial
+  property :goal, String
+  property :body, Text
+  property :created_at, DateTime
+  belongs_to :user
+  has n, :days
+end
+
+class Day
+  include DataMapper::Resource
+  property :id, Serial
+  property :marked, Boolean, :default  => false
+  belongs_to :calendar
+end
 
 get "/" do
   erb :index
@@ -13,3 +42,5 @@ post "/calendar" do
   @goal = params[:goal]
   erb :calendar
 end
+
+DataMapper.auto_upgrade!
